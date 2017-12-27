798a25bd0094cfe9bbe41431cb214f7c2eaca4b2
Vincent van Dijk <info@vincentvandijk.com>
2017-12-27 15:06:01 +0100

    Changed return type and added method.

--- src/Framework/ServiceLocator/Factory/Logger/LoggerFactory.php
+++ src/Framework/ServiceLocator/Factory/Logger/LoggerFactory.php
@@ -5,7 +5,9 @@ namespace ExtendsFramework\Logger\Framework\ServiceLocator\Factory\Logger;
 
 use ExtendsFramework\Logger\Logger;
 use ExtendsFramework\Logger\LoggerInterface;
+use ExtendsFramework\Logger\Writer\WriterInterface;
 use ExtendsFramework\ServiceLocator\Resolver\Factory\ServiceFactoryInterface;
+use ExtendsFramework\ServiceLocator\ServiceLocatorException;
 use ExtendsFramework\ServiceLocator\ServiceLocatorInterface;
 
 class LoggerFactory implements ServiceFactoryInterface
@@ -13,7 +15,7 @@ class LoggerFactory implements ServiceFactoryInterface
     /**
      * @inheritDoc
      */
-    public function createService(string $key, ServiceLocatorInterface $serviceLocator, array $extra = null): LoggerInterface
+    public function createService(string $key, ServiceLocatorInterface $serviceLocator, array $extra = null): object
     {
         $config = $serviceLocator->getConfig();
         $config = $config[LoggerInterface::class] ?? [];
@@ -21,10 +23,24 @@ class LoggerFactory implements ServiceFactoryInterface
         $logger = new Logger();
         foreach ($config['writers'] ?? [] as $writer) {
             $logger->addWriter(
-                $serviceLocator->getService($writer['name'], $writer['options'] ?? [])
+                $this->getWriter($serviceLocator, $writer['name'], $writer['options'] ?? [])
             );
         }
 
         return $logger;
+    }
+
+    /**
+     * Get writer for name with options.
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @param string                  $name
+     * @param array                   $options
+     * @return WriterInterface
+     * @throws ServiceLocatorException
+     */
+    protected function getWriter(ServiceLocatorInterface $serviceLocator, string $name, array $options): object
+    {
+        return $serviceLocator->getService($name, $options);
     }
 }
