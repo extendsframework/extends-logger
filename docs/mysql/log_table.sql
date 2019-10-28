491156933c4e3c492f5fbadca9351845fbb02c16
Vincent van Dijk <info@vincentvandijk.com>
2019-10-28 18:16:53 +0100

    Reformatted SQL and removed auto increment.

--- docs/mysql/log_table.sql
+++ docs/mysql/log_table.sql
@@ -1,9 +1,11 @@
-CREATE TABLE `log` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `value` tinyint(1) unsigned NOT NULL,
-  `keyword` varchar(8) NOT NULL DEFAULT '',
-  `date_time` datetime NOT NULL,
-  `message` varchar(1024) NOT NULL DEFAULT '',
-  `meta_data` json DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+CREATE TABLE `log`
+(
+    `id`        int(11) unsigned    NOT NULL AUTO_INCREMENT,
+    `value`     tinyint(1) unsigned NOT NULL,
+    `keyword`   varchar(8)          NOT NULL DEFAULT '',
+    `date_time` datetime            NOT NULL,
+    `message`   varchar(1024)       NOT NULL DEFAULT '',
+    `meta_data` json                         DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
